d5cfbe0764ee1e9204a8f50848a94fb9a5687b7c
hemingyang <hemingyang@xiaochankeji.cn>
2021-05-14 18:04:52 +0800

    ios依赖

---
+++ Target Support Files/fl_umeng_share/fl_umeng_share-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_fl_umeng_share : NSObject
+@end
+@implementation PodsDummy_fl_umeng_share
+@end
---
+++ Target Support Files/flutter_alibc/flutter_alibc-umbrella.h
@@ -0,0 +1,24 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "FlutterAlibcConstKey.h"
+#import "FlutterAlibcPlugin.h"
+#import "FlutterAlibcHandle.h"
+#import "FlutterAlibcTools.h"
+#import "ALiAuthWebViewController.h"
+#import "ALiTradeWebViewController.h"
+#import "FlutterWxViewCtrlViewController.h"
+#import "PrefixHeader.h"
+
+FOUNDATION_EXPORT double flutter_alibcVersionNumber;
+FOUNDATION_EXPORT const unsigned char flutter_alibcVersionString[];
+
